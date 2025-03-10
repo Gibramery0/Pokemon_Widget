1e14824d9e6ca230aea3c4fc99d0429b5a5c747e
Gibramery0 <ibrayanar2bin5@gamil.com>
2025-03-10 05:09:52 -0700

    Initial Commit

---
+++ PokemonWidget/PokemonWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidgetApp.swift
+//  PokemonWidget
+//
+//  Created by Gibramery on 10.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
